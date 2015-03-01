4bf19311362a5911fc033c7c981ad714502010bb
Cynthia Yeung <cyeung91@gmail.com>
2015-03-01 15:59:36 -0800

    Access controller and views for user login

---
+++ itinerary-planner-project/app/controllers/access_controller.rb
@@ -0,0 +1,70 @@
+class AccessController < ApplicationController
+  skip_before_action :confirm_logged_in, only: [:signup, :login, :create, :attempt_login]
+  before_action :prevent_login_signup, only: [:signup, :login]
+
+  def signup
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "You are now logged in!"
+      redirect_to itineraries_path(session[:user_id]) #(root_path) when user is created, redirect to all itineraries specific to user
+    else
+      render :signup
+    end
+  end
+
+  def login
+  end
+
+  def attempt_login
+
+    if params[:username].present? && params[:password].present?
+      found_user = User.find_by_username params[:username]
+      if found_user
+        authorized_user = found_user.confirm params[:password]
+      end
+    end
+
+    if !found_user
+      flash.now[:alert] = "Invalid username"
+      render :login
+
+    elsif !authorized_user
+      flash.now[:alert] = "Invalid password"
+      render :login
+
+    else
+      session[:user_id] = authorized_user.id
+      redirect_to itineraries_path, flash: {success: "You are now logged in."}
+      # (root_path) probably going to break—when successfully logged in
+      # this needs to route to itineraries page of the session user
+    end
+
+  end
+
+  def home
+
+  end
+
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to login_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_name, :password, :password_digest, :first_name, :last_name, :email)
+  end
+
+  def prevent_login_signup
+    if session[:user_id]
+      redirect_to home_path
+    end
+  end
+end
--- itinerary-planner-project/config/routes.rb
+++ itinerary-planner-project/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'access/home'
+
+  get 'access/login'
+
+  get 'access/signup'
+
   get 'login', to: "access#login", as: 'login'
 
   get 'signup', to: "access#signup", as: 'signup'
@@ -19,16 +25,16 @@ Rails.application.routes.draw do
   post '/actors/:id/movies/new' => 'actors#add_movie', as: :add_movie
   delete '/actors/:id/movies/:movie_id' => 'actors#remove_movie', as: :remove_movie
 
-  root 'site#index'
+  root 'access#login'
 
   # Adding and removing destination to/from a single itinerary
   post '/itineraries/:id/destinations/new' => 'destinations#create', as: :add_destination
   delete '/itineraries/:id/destinations/:destination_id' => 'destinations#destroy', as: :remove_destination
 
   # Adding and removing a user/companion to/from an existing destination
-  post '/itineraries/:id/destinations/new' => 'companions#create', as: :add_companion
-  post '/itineraries/:id/destinations/:destination_id/edit' => 'companions#create', as: :add_companion
-  delete '/itineraries/:id/destinations/:destination_id' => 'companions#destroy', as: :remove_companion
-  delete '/itineraries/:id/destinations/:destination_id/edit' => 'companions#destroy', as: :remove_companion
+  # post '/itineraries/:id/destinations/new' => 'companions#create', as: :add_companion
+  # post '/itineraries/:id/destinations/:destination_id/edit' => 'companions#create', as: :add_companion
+  # delete '/itineraries/:id/destinations/:destination_id' => 'companions#destroy', as: :remove_companion
+  # delete '/itineraries/:id/destinations/:destination_id/edit' => 'companions#destroy', as: :remove_companion
 
 end
