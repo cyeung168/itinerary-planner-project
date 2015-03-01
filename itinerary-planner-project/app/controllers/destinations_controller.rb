2a4a0054af47142c1fee1af8d591cb2c1d6a1a2d
Christine Lau <chylau.design@gmail.com>
2015-02-28 23:40:35 -0800

    Added comments and destinations controllers, Added views for destinations

---
+++ itinerary-planner-project/app/controllers/destinations_controller.rb
@@ -0,0 +1,73 @@
+class DestinationsController < ApplicationController
+before_action :find_destination, :find_itinerary, only: [:edit, :show, :update, :destroy, :add_user, :remove_user]
+
+
+  def index
+    @destinations = Destination.all
+  end
+
+  def show
+    @users= User.all - @destination.users
+  end
+
+  def new
+    @destination = Destination.new
+  end
+
+  def edit
+  end
+
+  def create
+    Destination.create destination_params
+    redirect_to @itinerary
+  end
+
+  def update
+    @destination.update_attributes destination_params
+    redirect_to @itinerary
+  end
+
+  def destroy
+    @destination.destroy
+    redirect_to @itinerary
+  end
+
+  def add_user
+    @destination = Destination.find(params[:id])
+    @user = User.find(params[:user_id])
+    @itinerary = Itinerary.find(params[:itinerary_id])
+    unless @destination.users.include? user
+      @destination.users << user
+      @itinerary.users << user
+    else
+      redirect_to @destination
+    end
+  end
+
+  def remove_user
+    user = User.find(params[:user_id])
+    @destination.users.delete(user)
+    redirect_to @destination
+  end
+
+private
+  def destination_params
+    params.require(:destination).permit(:id, :location, :name, :info)
+  end
+
+  def itinerary_params
+    params.require(:itinerary).permit(:id, :origin, :destination, :name, :depart_date, :return_date)
+  end
+
+  def find_destination
+    @destination = Destination.find(params[:id])
+  end
+
+  def find_itinerary
+    @itinerary = Itinerary.find(params[:itinerary_id])
+  end
+
+  #   def find_user
+  #   @user = User.find(params[:itinerary_id])
+  # end
+end
--- itinerary-planner-project/config/routes.rb
+++ itinerary-planner-project/config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'destinations/index'
+
+  get 'destinations/show'
+
+  get 'destinations/new'
+
+  get 'destinations/edit'
+
+root 'site#index'
+
+
   get 'login', to: "access#login", as: 'login'
 
   get 'signup', to: "access#signup", as: 'signup'
@@ -16,19 +27,15 @@ Rails.application.routes.draw do
     post '/comments' => 'comments#create', as: 'comments'
   end
 
-  post '/actors/:id/movies/new' => 'actors#add_movie', as: :add_movie
-  delete '/actors/:id/movies/:movie_id' => 'actors#remove_movie', as: :remove_movie
 
-  root 'site#index'
+  # # Adding and removing destination to/from a single itinerary
+  # post '/itineraries/:id/destinations/new' => 'destinations#create', as: :add_destination
+  # delete '/itineraries/:id/destinations/:destination_id' => 'destinations#destroy', as: :remove_destination
 
-  # Adding and removing destination to/from a single itinerary
-  post '/itineraries/:id/destinations/new' => 'destinations#create', as: :add_destination
-  delete '/itineraries/:id/destinations/:destination_id' => 'destinations#destroy', as: :remove_destination
-
-  # Adding and removing a user/companion to/from an existing destination
-  post '/itineraries/:id/destinations/new' => 'companions#create', as: :add_companion
-  post '/itineraries/:id/destinations/:destination_id/edit' => 'companions#create', as: :add_companion
-  delete '/itineraries/:id/destinations/:destination_id' => 'companions#destroy', as: :remove_companion
-  delete '/itineraries/:id/destinations/:destination_id/edit' => 'companions#destroy', as: :remove_companion
+  # # Adding and removing a user/companion to/from an existing destination
+  # post '/itineraries/:id/destinations/new' => 'companions#create', as: :add_companion
+  # post '/itineraries/:id/destinations/:destination_id/edit' => 'companions#create', as: :add_companion
+  # delete '/itineraries/:id/destinations/:destination_id' => 'companions#destroy', as: :remove_companion
+  # delete '/itineraries/:id/destinations/:destination_id/edit' => 'companions#destroy', as: :remove_companion
 
 end
